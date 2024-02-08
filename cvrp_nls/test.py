88d4fc4c5671b41bbb321875c48bfb1b557ff836
hyeok9855 <hyeok9855@gmail.com>
2024-02-09 02:30:30 +0900

    add gfacs for cvrp_nls

---
+++ cvrp_nls/test.py
@@ -0,0 +1,183 @@
+import os
+import random
+import time
+
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import torch
+
+from net import Net
+from aco import ACO, get_subroutes
+from utils import load_test_dataset
+
+from typing import Tuple, List
+
+
+EPS = 1e-10
+
+
+def validate_route(distance: torch.Tensor, demands: torch.Tensor, routes: List[torch.Tensor]) -> Tuple[bool, float]:
+    length = 0.0
+    valid = True
+    visited = {0}
+    for r in routes:
+        d = demands[r].sum().item()
+        if d>1.000001:
+            valid = False
+        length += distance[r[:-1], r[1:]].sum().item()
+        for i in r:
+            i = i.item()
+            if i<0 or i>=distance.size(0):
+                valid = False
+            else:
+                visited.add(i)  # type: ignore
+    if len(visited) != distance.size(0):
+        valid = False
+    return valid, length
+
+
+@torch.no_grad()
+def infer_instance(model, pyg_data, demands, distances, positions, n_ants, t_aco_diff):
+    if model is not None:
+        model.eval()
+        heu_vec = model(pyg_data)
+        heu_mat = model.reshape(pyg_data, heu_vec) + EPS
+    else:
+        heu_mat = None
+
+    aco = ACO(
+        n_ants=n_ants,
+        heuristic=heu_mat.cpu() if heu_mat is not None else heu_mat,
+        demand=demands.cpu(),
+        distances=distances.cpu(),
+        device='cpu',
+        swapstar=True,
+        positions=positions.cpu(),
+    )
+
+    results = torch.zeros(size=(len(t_aco_diff),))
+    diversities = torch.zeros(size=(len(t_aco_diff),))
+    for i, t in enumerate(t_aco_diff):
+        results[i], diversities[i] = aco.run(t, inference=True)
+        path = get_subroutes(aco.shortest_path)
+        valid, length = validate_route(distances, demands, path)
+        assert (length - results[i].item()) < 1e-5  # FIXME: remove this line
+        if valid is False:
+           print("invalid solution.")
+    return results, diversities
+
+
+@torch.no_grad()
+def test(dataset, model, n_ants, t_aco):
+    _t_aco = [0] + t_aco
+    t_aco_diff = [_t_aco[i + 1] - _t_aco[i] for i in range(len(_t_aco) - 1)]
+
+    sum_results = torch.zeros(size=(len(t_aco_diff),))
+    sum_diversities = torch.zeros(size=(len(t_aco_diff),))
+    start = time.time()
+    for pyg_data, demands, distances, positions in tqdm(dataset):
+        results, diversities = infer_instance(
+            model, pyg_data, demands, distances, positions, n_ants, t_aco_diff
+        )
+        sum_results += results
+        sum_diversities += diversities
+    end = time.time()
+    return sum_results / len(dataset), sum_diversities / len(dataset), end - start
+
+
+def main(ckpt_path, n_nodes, k_sparse, size=None, n_ants=100, n_iter=10, guided_exploration=False, seed=0):
+    test_list = load_test_dataset(n_nodes, k_sparse, DEVICE, TAM)
+    test_list = test_list[:(size or len(test_list))]
+
+    t_aco = list(range(1, n_iter + 1))
+    print("problem scale:", n_nodes)
+    print("checkpoint:", ckpt_path)
+    print("number of instances:", size)
+    print("device:", 'cpu' if DEVICE == 'cpu' else DEVICE+"+cpu" )
+    print("n_ants:", n_ants)
+    print("seed:", seed)
+
+    if ckpt_path is not None:
+        net = Net(gfn=True, Z_out_dim=2 if guided_exploration else 1).to(DEVICE)
+        net.load_state_dict(torch.load(ckpt_path, map_location=DEVICE))
+    else:
+        net = None
+    avg_cost, avg_diversity, duration = test(test_list, net, n_ants, t_aco)
+    print('total duration: ', duration)
+    for i, t in enumerate(t_aco):
+        print(f"T={t}, avg. cost {avg_cost[i]}, avg. diversity {avg_diversity[i]}")
+
+    # Save result in directory that contains model_file
+    filename = os.path.splitext(os.path.basename(ckpt_path))[0] if ckpt_path is not None else 'none'
+    dirname = os.path.dirname(ckpt_path) if ckpt_path is not None else f'../pretrained/cvrp_nls/{args.nodes}/no_model'
+    os.makedirs(dirname, exist_ok=True)
+
+    result_filename = f"test_result_ckpt{filename}-cvrp{n_nodes}-ninst{size}-nants{n_ants}-niter{n_iter}-seed{seed}"
+    result_file = os.path.join(dirname, result_filename + ".txt")
+    with open(result_file, "w") as f:
+        f.write(f"problem scale: {n_nodes}\n")
+        f.write(f"checkpoint: {ckpt_path}\n")
+        f.write(f"number of instances: {len(test_list)}\n")
+        f.write(f"device: {'cpu' if DEVICE == 'cpu' else DEVICE+'+cpu'}\n")
+        f.write(f"n_ants: {n_ants}\n")
+        f.write(f"seed: {seed}\n")
+        f.write(f"total duration: {duration}\n")
+        for i, t in enumerate(t_aco):
+            f.write(f"T={t}, avg. cost {avg_cost[i]}, avg. diversity {avg_diversity[i]}\n")
+
+    results = pd.DataFrame(columns=['T', 'avg_cost', 'avg_diversity'])
+    results['T'] = t_aco
+    results['avg_cost'] = avg_cost
+    results['avg_diversity'] = avg_diversity
+    results.to_csv(os.path.join(dirname, result_filename + ".csv"), index=False)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nodes", type=int, help="Problem scale")
+    parser.add_argument("-k", "--k_sparse", type=int, default=None, help="k_sparse")
+    parser.add_argument("-p", "--path", type=str, default=None, help="Path to checkpoint file")
+    parser.add_argument("-i", "--n_iter", type=int, default=10, help="Number of iterations of ACO to run")
+    parser.add_argument("-n", "--n_ants", type=int, default=100, help="Number of ants")
+    parser.add_argument("-d", "--device", type=str,
+                        default=("cuda:0" if torch.cuda.is_available() else "cpu"), 
+                        help="The device to train NNs")
+    parser.add_argument("-s", "--size", type=int, default=None, help="Number of instances to test")
+    ### GFACS
+    parser.add_argument("--disable_guided_exp", action='store_true', help='True for model w/o guided exploration.')
+    ### Seed
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    ### Dataset
+    parser.add_argument("--tam", action="store_true", help="Use TAM dataset")
+
+    args = parser.parse_args()
+
+    if args.k_sparse is None:
+        args.k_sparse = args.nodes // 5
+
+    DEVICE = args.device if torch.cuda.is_available() else 'cpu'
+    TAM = args.tam
+
+    # seed everything
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    if args.path is not None and not os.path.isfile(args.path):
+        print(f"Checkpoint file '{args.path}' not found!")
+        exit(1)
+
+    main(
+        args.path,
+        args.nodes,
+        args.k_sparse,
+        args.size,
+        args.n_ants,
+        args.n_iter,
+        not args.disable_guided_exp,
+        args.seed
+    )
---
+++ cvrp_nls/utils.py
@@ -0,0 +1,154 @@
+import os
+import torch
+from torch_geometric.data import Data
+
+
+DEMAND_LOW = 1
+DEMAND_HIGH = 9
+
+
+def get_capacity(n: int, tam=False):
+    if tam:
+        capacity_list_tam = [
+            (1, 10), (20, 30), (50, 40), (100, 50), (400, 150), (1000, 200), (2000, 300)  # (number of nodes, capacity)
+        ]
+        return list(filter(lambda x: x[0]<=n, capacity_list_tam))[-1][-1]
+
+    capacity_dict = {
+        10: 20.,
+        20: 30.,
+        50: 40.,
+        100: 50.,
+        200: 50.,
+        500: 50.,
+        1000: 50.,
+        2000: 50.,
+    }
+    assert n in capacity_dict
+    return capacity_dict[n]
+
+
+def gen_instance(n, device, tam=False):
+    """
+    Implements data-generation method as described by Kool et al. (2019), Hou et al. (2023), and Son et al. (2023)
+
+    * Kool, W., van Hoof, H., & Welling, M. (2019). Attention, Learn to Solve Routing Problems! (arXiv:1803.08475)
+    * Hou, Q., Yang, J., Su, Y., Wang, X., & Deng, Y. (2023, February 1). Generalize Learned Heuristics to Solve Large-scale Vehicle Routing Problems in Real-time. The Eleventh International Conference on Learning Representations. https://openreview.net/forum?id=6ZajpxqTlQ
+    * Son, J., et al. (2023). Meta-SAGE: Scale Meta-Learning Scheduled Adaptation with Guided Exploration for Mitigating Scale Shift on Combinatorial Optimization (arXiv:2306.02688)
+    """
+    locations = torch.rand(size=(n + 1, 2), device=device, dtype=torch.double)
+    demands = torch.randint(low=DEMAND_LOW, high=DEMAND_HIGH + 1, size=(n, ), device=device, dtype=torch.double)
+    demands_normalized = demands / get_capacity(n, tam)
+    all_demands = torch.cat((torch.zeros((1, ), device=device, dtype=torch.double), demands_normalized))
+    distances = gen_distance_matrix(locations)
+    return all_demands, distances, locations
+
+
+def gen_distance_matrix(tsp_coordinates):
+    n_nodes = len(tsp_coordinates)
+    distances = torch.norm(tsp_coordinates[:, None] - tsp_coordinates, dim=2, p=2, dtype=torch.double)
+    distances[torch.arange(n_nodes), torch.arange(n_nodes)] = 1e-10  # note here
+    return distances
+
+
+def gen_pyg_data(demands, distances, device, k_sparse):
+    n = demands.size(0)
+    # First mask out self-loops by setting them to large values
+    temp_dists = distances.clone()
+    temp_dists[1:, 1:][torch.eye(n - 1, dtype=torch.bool, device=device)] = 1e9
+    # sparsify
+    # part 1:
+    topk_values, topk_indices = torch.topk(temp_dists[1:, 1:], k = k_sparse, dim=1, largest=False)
+    edge_index_1 = torch.stack([
+        torch.repeat_interleave(torch.arange(n-1).to(topk_indices.device), repeats=k_sparse),
+        torch.flatten(topk_indices)
+    ]) + 1
+    edge_attr_1 = topk_values.reshape(-1, 1)
+    # part 2: keep all edges connected to depot
+    edge_index_2 = torch.stack([ 
+        torch.zeros(n - 1, device=device, dtype=torch.long), 
+        torch.arange(1, n, device=device, dtype=torch.long),
+    ])
+    edge_attr_2 = temp_dists[1:, 0].reshape(-1, 1)
+    edge_index_3 = torch.stack([ 
+        torch.arange(1, n, device=device, dtype=torch.long),
+        torch.zeros(n - 1, device=device, dtype=torch.long), 
+    ])
+    edge_index = torch.concat([edge_index_1, edge_index_2, edge_index_3], dim=1)
+    edge_attr = torch.concat([edge_attr_1, edge_attr_2, edge_attr_2])
+
+    x = demands
+    # FIXME: append node type and coordinates into x
+    pyg_data = Data(x=x.unsqueeze(1).float(), edge_attr=edge_attr.float(), edge_index=edge_index)
+    return pyg_data
+
+
+def load_test_dataset(n_node, k_sparse, device, tam=False):
+    filename = f"../data/cvrp/testDataset-{'tam-' if tam else ''}{n_node}.pt"
+    if not os.path.isfile(filename):
+        raise FileNotFoundError(
+            f"File {filename} not found, please download the test dataset from the original repository."
+        )
+    dataset = torch.load(filename, map_location=device)
+
+    test_list = []
+    for i in range(len(dataset)):
+        demands, position, distances = dataset[i, 0, :], dataset[i, 1:3, :], dataset[i, 3:, :]
+        pyg_data = gen_pyg_data(demands, distances, device, k_sparse=k_sparse)
+        test_list.append((pyg_data, demands, distances, position.T))
+    return test_list
+
+
+def load_val_dataset(n_node, k_sparse, device, tam=False):
+    filename = f"../data/cvrp/valDataset-{'tam-' if tam else ''}{n_node}.pt"
+    if not os.path.isfile(filename):
+        dataset = []
+        for i in range(50):
+            demand, dist, position = gen_instance(n_node, device, tam)  # type: ignore
+            instance = torch.vstack([demand, position.T, dist])
+            dataset.append(instance)
+        dataset = torch.stack(dataset)
+        torch.save(dataset, filename)
+    else:
+        dataset = torch.load(filename, map_location=device)
+
+    val_list = []
+    for i in range(len(dataset)):
+        demands, position, distances = dataset[i, 0, :], dataset[i, 1:3, :], dataset[i, 3:, :]
+        pyg_data = gen_pyg_data(demands, distances, device, k_sparse=k_sparse)
+        val_list.append((pyg_data, demands, distances, position.T))
+    return val_list
+
+
+if __name__ == '__main__':
+    import pathlib
+    import pickle
+    pathlib.Path('../data/cvrp').mkdir(exist_ok=True)
+
+    # TAM dataset
+    torch.manual_seed(123456)
+    for n in [100, 400, 1000]:  # problem scale
+        inst_list = []
+        for _ in range(100):
+            demand, dist, position = gen_instance(n, 'cpu', tam=True)  # type: ignore
+            instance = torch.vstack([demand, position.T, dist])
+            inst_list.append(instance)
+        testDataset = torch.stack(inst_list)
+        torch.save(testDataset, f'../data/cvrp/testDataset-tam-{n}.pt')
+
+    # main Dataset
+    for scale in [200, 500, 1000]:
+        with open(f"../data/cvrp/vrp{scale}_128.pkl", "rb") as f:
+            dataset = pickle.load(f)
+
+        inst_list = []
+        for instance in dataset:
+            depot_position, positions, demands, capacity = instance
+
+            demands_torch = torch.tensor([0] + [d / capacity for d in demands], dtype=torch.float64)
+            positions_torch = torch.tensor([depot_position] + positions, dtype=torch.float64)
+            distmat_torch = gen_distance_matrix(positions_torch)
+            inst_list.append(torch.vstack([demands_torch, positions_torch.T, distmat_torch]))
+
+            test_dataset = torch.stack(inst_list)
+            torch.save(test_dataset, f"../data/cvrp/testDataset-{scale}.pt")
